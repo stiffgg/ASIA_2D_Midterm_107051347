2dfec7c7b04674b698ebede7b351aabbbcf2b95c
stiffgg <71679936+stiffgg@users.noreply.github.com>
2020-11-16 21:48:19 +0800

    完成
    結束

--- ASIA_2D_Midterm_107051347/Assets/scripts/Player.cs
+++ ASIA_2D_Midterm_107051347/Assets/scripts/Player.cs
@@ -1,18 +1,26 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+
+    public Text textCount;
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "傳送門")
         {
             final.SetActive(true);
         }
-        if (collision.name == "鳳梨")
+        if (collision.tag == "鳳梨")
         {
             Destroy(collision.gameObject);
+            count++;
+            textCount.text = "鳳梨數量:" + count;
+
         }
     }
     
